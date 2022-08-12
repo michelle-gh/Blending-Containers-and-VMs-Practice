9e51cfeab1b6f6f4348ab2af9a20c03715d0b111
nijama <nijama.dev@gmail.com>
2022-08-12 12:31:05 -0500

    Revert "Update 00b_get-kernel-and-rootfs.sh"
    This reverts commit 44709254e9a0378c4e0db740c19ae55219eee32b.

--- Firecracker/Scripts/00b_get-kernel-and-rootfs.sh
+++ Firecracker/Scripts/00b_get-kernel-and-rootfs.sh
@@ -3,6 +3,6 @@
 git clone https://github.com/bkleiner/ubuntu-firecracker && \
 cd ubuntu-firecracker && \
 docker build -t ubuntu-firecracker . && \
-mkdir output && \
+sleep 2 && \
 docker run --privileged -it --rm -v $(pwd)/output:/output ubuntu-firecracker && \
 cd ..
