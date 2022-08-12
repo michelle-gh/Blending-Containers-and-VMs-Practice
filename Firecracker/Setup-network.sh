34a722753997dbd2e5eae75eebcaf3cddd56efe5
nijama <nijama.dev@gmail.com>
2022-08-12 12:45:52 -0500

    no err show

--- Firecracker/Setup-network.sh
+++ Firecracker/Setup-network.sh
@@ -1,7 +1,7 @@
 DEVICE_NAME=docker0
 VIRTUAL_DEVICE=veth0
 # Remove older instances
-sudo ip link del $VIRTUAL_DEVICE
+sudo ip link del $VIRTUAL_DEVICE 2> /dev/null
 # Add new virtual device
 sudo ip tuntap add $VIRTUAL_DEVICE mode tap # user $(id -u) group $(id -g)
 sudo brctl addif $DEVICE_NAME $VIRTUAL_DEVICE
