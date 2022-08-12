80448f057ba075db37a131b42d9536f8b5850ba4
nijama <nijama.dev@gmail.com>
2022-08-12 12:10:40 -0500

    test

--- Firecracker/Scripts/00b_get-kernel-and-rootfs.sh
+++ Firecracker/Scripts/00b_get-kernel-and-rootfs.sh
@@ -2,6 +2,8 @@
 
 git clone https://github.com/bkleiner/ubuntu-firecracker && \
 cd ubuntu-firecracker && \
+echo "AAAAAAAAAAAA $(pwd)" && \
 docker build -t ubuntu-firecracker . && \
+echo "BBBBBBBBBBBB" && \
 docker run --privileged -it --rm -v $(pwd)/output:/output ubuntu-firecracker && \
 cd ..
