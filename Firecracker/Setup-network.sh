30b78cf87a73d0ca76ed87faca9fcc0192947add
nijama <nijama.dev@gmail.com>
2022-08-12 11:14:21 -0500

    Update Setup-network.sh

--- Firecracker/Setup-network.sh
+++ Firecracker/Setup-network.sh
@@ -1,6 +1,8 @@
-sudo ip link del veth0
-
-DEVICE_NAME="docker0"
-sudo ip tuntap add veth0 mode tap # user $(id -u) group $(id -g)
-sudo brctl addif docker0 veth0
-sudo ip link set veth0 up
+DEVICE_NAME=docker0
+VIRTUAL_DEVICE=veth0
+# Remove older instances
+sudo ip link del $VIRTUAL_DEVICE
+# Add new virtual device
+sudo ip tuntap add $VIRTUAL_DEVICE mode tap # user $(id -u) group $(id -g)
+sudo brctl addif $DEVICE_NAME $VIRTUAL_DEVICE
+sudo ip link set $VIRTUAL_DEVICE up
